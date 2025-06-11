c7bacefa2d491d65108dab2c85756f7a220ab712
Luizabarbosa6 <153461981+Luizabarbosa6@users.noreply.github.com>
2025-06-11 17:41:49 -0300

    Ajustando o env

--- services/geminiService.js
+++ services/geminiService.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 async function consultarGemini(prompt) {
-  const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCeSQLUxl23TWHo0Bh0gBm5_W6pWNeX2RQ";
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
   const body = {
     contents: [
